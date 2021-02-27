7da76e8cb8098346453c45755476a8652f665982
EnidFerns <69514959+EnidFerns@users.noreply.github.com>
2021-02-27 21:43:15 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,16 @@
+//6 stairs pattern
+#include <stdio.h>
+
+int main() 
+{
+   int i, j;
+   for (i = 0; i < 6; i++) 
+   {	
+      for (j = 0; j <= i; j++) 
+	  {
+         printf("0 1 ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
